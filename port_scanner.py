608f7855c37083d297dda27ca4deae7dcc8151ad
tomek <luci6969@protonmail.ch>
2022-05-08 12:16:35 +0200

    Add service name with port

--- port_scanner.py
+++ port_scanner.py
@@ -3,7 +3,6 @@ server_ips = []
 with open("ips") as f:
     for ip in f.readlines():
         server_ips.append(ip.rstrip())
-
 for ip in server_ips:
     scanner = socket.gethostbyname(ip)
     with open("open_ports.txt", "a", encoding="utf-8") as f:
@@ -13,8 +12,9 @@ for ip in server_ips:
         print(f"Scanning port: {port}")
         open_ports = sock.connect_ex((ip, port))
         if open_ports == 0:
+            service_name = socket.getservbyport(port, "tcp")
             with open("open_ports.txt", "a", encoding="UTF-8") as f:
-                f.write(f"{port} ")
+                f.write(f"{port} {service_name} ")
                 sock.close()
     with open("open_ports.txt", "a", encoding="utf-8") as f:
         f.write(f"\n")
